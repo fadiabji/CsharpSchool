f74b96a211c5a9a377f35c57b96052fe4dbc683e
fadiabji <fadi.abji@htomail.com>
2022-09-27 11:29:23 +0200

    Add project files.

---
+++ oopAssignment2/Classes/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopAssignment2.Classes
+{
+    internal class Student
+    {
+        [primerKey]
+        public int StudentId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string fname, string lname, DateTime dateOfBirth)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "First Name : " + Firstname + "|| Last Name : " + Lastname + "\nDate Of Birth : " + DateOfBirth + "|| Student Id: " + StudentId.ToString();
+        }
+
+        public void GetAge()
+        {
+            
+        }
+
+
+    }
+}
